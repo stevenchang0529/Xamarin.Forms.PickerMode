0ff5808b328658456ae421c93377cd52e5567b23
Steven <steven@MSI-Steven>
2018-12-30 08:51:44 +0800

    加入專案檔案。

---
+++ Xamarin.Forms.PickerMode/Xamarin.Forms.PickerMode/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+namespace Xamarin.Forms.PickerMode
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            On<iOS>().SetUseSafeArea(true);
+            picker.On<iOS>().SetUpdateMode(UpdateMode.WhenFinished);
+        }
+    }
+}
